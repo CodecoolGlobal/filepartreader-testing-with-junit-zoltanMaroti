4eba3f531e8c32abfffe752d02c9a5898c17bf11
zoltanMaroti <zoli.maroti@gmail.com>
2019-09-30 15:36:36 +0200

    first assertion test added

---
+++ src/test/FilePartReaderTest.java
@@ -0,0 +1,28 @@
+package main.java.com.codecool.filePartReader;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilePartReaderTest {
+
+    @Test
+    public void testReader() throws IOException {
+        FilePartReader filePartReader = new FilePartReader();
+        filePartReader.setup("/Users/Home/Codecool/OOP/5th-SI-week/filepartreader-testing-with-junit-zoltanMaroti/src/main/resources/text.txt", 1, 1);
+
+        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+                "Integer tempor pulvinar nunc, et malesuada quam convallis blandit.\n" +
+                "Proin blandit pellentesque nulla nec aliquam.\n" +
+                "Ut ac tincidunt erat. Donec sed eleifend dolor.\n" +
+                "In justo mauris, laoreet eu urna sit amet, egestas porttitor arcu.\n" +
+                "Morbi pharetra, neque vitae pellentesque posuere, justo dui suscipit justo, vitae convallis eros felis sed arcu.\n" +
+                "Pellentesque sit amet ultrices erat, nec aliquam nulla.\n" +
+                "Proin et tellus non quam interdum luctus nec quis lectus.\n" +
+                "Praesent porttitor urna et elit egestas, at rutrum nisl vehicula.\n" +
+                "Proin gravida interdum rhoncus. Pellentesque efficitur neque ut viverra volutpat.\n", filePartReader.read());
+    }
+
+}
