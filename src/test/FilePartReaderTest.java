7f2a41fe0db98646eaaa669d1a046fd43eea053e
zoltanMaroti <zoli.maroti@gmail.com>
2019-10-01 13:02:02 +0200

    FilePartReader tests refactored

--- src/test/FilePartReaderTest.java
+++ src/test/FilePartReaderTest.java
@@ -2,28 +2,26 @@ package main.java.com.codecool.filePartReader;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePartReaderTest {
 
+    FilePartReader filePartReader = new FilePartReader();
+
     @Test
     public void shouldThrowIllegalArgumentExceptionWhenToLineIsSmallerThenFromLine() {
-        FilePartReader filePartReader = new FilePartReader();
         assertThrows(IllegalArgumentException.class, () -> filePartReader.setup("/Users/Home/Codecool/OOP/5th-SI-week/filepartreader-testing-with-junit-zoltanMaroti/src/main/resources/text.txt", 5, 1));
     }
 
     @Test
     public void shouldThrowIllegalArgumentExceptionWhenFromLineIsSmallerThen1() {
-        FilePartReader filePartReader = new FilePartReader();
         assertThrows(IllegalArgumentException.class, () -> filePartReader.setup("/Users/Home/Codecool/OOP/5th-SI-week/filepartreader-testing-with-junit-zoltanMaroti/src/main/resources/text.txt", - 1, 1));
     }
 
     @Test
     public void shouldThrowIOException() {
-        FilePartReader filePartReader = new FilePartReader();
         filePartReader.setup("resources/nope.txt", 1, 2);
         assertThrows(IOException.class, () -> filePartReader.read());
     }
@@ -31,13 +29,12 @@ class FilePartReaderTest {
 
     @Test
     public void testReader() throws IOException {
-        FilePartReader filePartReader = new FilePartReader();
         filePartReader.setup("/Users/Home/Codecool/OOP/5th-SI-week/filepartreader-testing-with-junit-zoltanMaroti/src/main/resources/text.txt", 1, 1);
 
         assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
                 "Integer tempor pulvinar nunc, et malesuada quam convallis blandit.\n" +
                 "Proin blandit pellentesque nulla nec aliquam.\n" +
-                "Ut ac tincidunt erat. Donec sed eleifend dolor.\n" +
+                "Ut ac tincidunt erat. Donec sed non eleifend dolor.\n" +
                 "In justo mauris, laoreet eu urna sit amet, egestas porttitor arcu.\n" +
                 "Morbi pharetra, neque vitae pellentesque posuere, justo dui suscipit justo, vitae convallis eros felis sed arcu.\n" +
                 "Pellentesque sit amet ultrices erat, nec aliquam nulla.\n" +
@@ -48,7 +45,6 @@ class FilePartReaderTest {
 
     @Test
     public void testReadLines() throws IOException {
-        FilePartReader filePartReader = new FilePartReader();
         filePartReader.setup("/Users/Home/Codecool/OOP/5th-SI-week/filepartreader-testing-with-junit-zoltanMaroti/src/main/resources/text.txt", 2, 2);
 
         assertEquals("Integer tempor pulvinar nunc, et malesuada quam convallis blandit.", filePartReader.readLines());
